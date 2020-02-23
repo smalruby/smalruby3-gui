063e58394da5e011e9889638878e3c056afa888f
Travis CI User <travis@example.org>
2020-02-23 11:49:40 +0000

    Build for ad9fbc8e65c54db1439a90629082782a272be5f7 [skip ci]

--- develop/extension-worker.js
+++ develop/extension-worker.js
@@ -360,7 +360,12 @@ var ArgumentType = {
   /**
    * MIDI note number with note picker (piano) field
    */
-  NOTE: 'note'
+  NOTE: 'note',
+
+  /**
+   * Inline image on block (as part of the label)
+   */
+  IMAGE: 'image'
 };
 module.exports = ArgumentType;
 
@@ -422,7 +427,7 @@ module.exports = BlockType;
 /* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
