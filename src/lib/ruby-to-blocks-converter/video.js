653044752e23f662ba5d541c7dd07534f34dd252
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-10-09 15:54:26 +0900

    video_motion_greater_thanの実装

--- src/lib/ruby-to-blocks-converter/video.js
+++ src/lib/ruby-to-blocks-converter/video.js
@@ -1,5 +1,5 @@
 /* global Opal */
-import _ from "lodash";
+import _ from 'lodash';
 
 /**
  * Video converter
@@ -10,52 +10,88 @@ const VideoConverter = {
         let block;
         if ((this._isSelf(receiver) || receiver === Opal.nil) && !rubyBlock) {
             switch (name) {
-                case "video_turn":
+                case 'video_turn':
                     if (args.length === 1 && this._isStringOrBlock(args[0])) {
                         block = this._createBlock(
-                            "videoSensing_videoToggle",
-                            "statement"
+                            'videoSensing_videoToggle',
+                            'statement'
                         );
                         this._addInput(
                             block,
-                            "VIDEO_STATE",
+                            'VIDEO_STATE',
                             this._createFieldBlock(
-                                "videoSensing_menu_VIDEO_STATE",
-                                "VIDEO_STATE",
+                                'videoSensing_menu_VIDEO_STATE',
+                                'VIDEO_STATE',
                                 args[0]
                             )
                         );
                     }
                     break;
-                case "video_motion":
+                case 'video_motion':
                     if (args.length === 0) {
                         block = this._createBlock(
-                            "videoSensing_videoOn",
-                            "value"
+                            'videoSensing_videoOn',
+                            'value'
                         );
                         // this._addInput(
                         //     block,
-                        //     "ATTRIBUTE",
+                        //     'ATTRIBUTE',
                         //     this._createFieldBlock(
-                        //         "videoSensing_menu_ATTRIBUTE",
-                        //         "ATTRIBUTE",
+                        //         'videoSensing_menu_ATTRIBUTE',
+                        //         'ATTRIBUTE',
                         //         args[0]
                         //     )
                         // );
                         // this._addInput(
                         //     block,
-                        //     "SUBJECT",
+                        //     'SUBJECT',
                         //     this._createFieldBlock(
-                        //         "videoSensing_menu_SUBJECT",
-                        //         "SUBJECT",
+                        //         'videoSensing_menu_SUBJECT',
+                        //         'SUBJECT',
                         //         args[0]
                         //     )
                         // );
                     }
                     break;
-                // case "":
-                //     if
-                //     break;
+                case 'video_transparency=':
+                    // done
+                    if (args.length === 1 && this._isNumberOrBlock(args[0])) {
+                        block = this._createBlock(
+                            'videoSensing_setVideoTransparency',
+                            'statement'
+                        );
+                        this._addNumberInput(
+                            block,
+                            'TRANSPARENCY',
+                            'math_number',
+                            args[0],
+                            50
+                        );
+                    }
+                    break;
+            }
+        } else if (
+            (this._isSelf(receiver) || receiver === Opal.nil) &&
+            name === 'when' &&
+            args.length == 2 &&
+            args[0].type === 'sym' &&
+            this._isNumberOrBlock(args[1]) &&
+            rubyBlockArgs &&
+            rubyBlockArgs.length === 0 &&
+            rubyBlock
+        ) {
+            switch (args[0].value) {
+                case 'video_motion_greater_than':
+                    console.log('video_motion_geater_than');
+                    if (args.length === 2 && this._isNumberOrBlock(args[1])) {
+                        block = this._createBlock(
+                            'videoSensing_whenMotionGreaterThan',
+                            'hat'
+                        );
+                        this._addNumberInput(block, 'REFERENCE', 'math_number', args[1], 10);
+                        this._setParent(rubyBlock, block);
+                    }
+                    break;
             }
         }
         return block;
