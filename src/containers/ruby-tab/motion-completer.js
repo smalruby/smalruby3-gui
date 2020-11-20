bc312df38ebbe1f0e5ceed3a2ae232cdd2af4f38
Kouji Takao <kouji.takao@gmail.com>
2020-11-20 17:46:57 +0900

    Update motion-completer.js

--- src/containers/ruby-tab/motion-completer.js
+++ src/containers/ruby-tab/motion-completer.js
@@ -5,21 +5,18 @@ class MotionCompleter {
     getCompletions (editor, session, pos, prefix, callback) {
         console.log("pos: ", pos);
         const words = [
-            "move(10)",
-            "turn_right(15)",
-            "turn_left(15)",
-            'go_to("_random_")',
-            "go_to([0, 0])",
-            'glide("_random_", secs: 1)',
-            "glide([0, 0], secs: 1)",
-            "self.direction = 90",
+            {
+                value: "move(10)",
+                meta: "10歩動かす" 
+            },
+            {
+                value: "turn_right(15)",
+                meta: "15度回す"
+            }
         ];
         let completions = [];
         words.forEach(function(w) {
-            completions.push({
-                value: w,
-                meta: "my completion"
-            });
+            completions.push(w);
         });
         callback(null, completions);
     }
