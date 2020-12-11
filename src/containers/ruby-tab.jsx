7f31691a1947ad935c3a1c38f2358e13930c6ece
Laminne Tatsuto Yamamoto <laminne33569@approvers.dev>
2020-12-11 17:16:44 +0900

    「制御」のブロックの補完の実装

--- src/containers/ruby-tab.jsx
+++ src/containers/ruby-tab.jsx
@@ -20,6 +20,7 @@ import 'brace/ext/language_tools';
 
 import MotionCompleter from './ruby-tab/motion-completer';
 import EventsCompleter from './ruby-tab/events-completer';
+import ControlCompleter from './ruby-tab/control-completer';
 
 class RubyTab extends React.Component {
     constructor (props) {
@@ -92,7 +93,8 @@ class RubyTab extends React.Component {
 
         const completers = [
             new MotionCompleter(),
-            new EventsCompleter()
+            new EventsCompleter(),
+            new ControlCompleter()
         ];
 
         return (
