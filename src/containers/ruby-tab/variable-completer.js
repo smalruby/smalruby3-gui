5f234d638cd6f7bd9fe154a2f2d5736c99329bcc
riversunlight <aabbee107@icloud.com>
2021-04-16 18:12:35 +0900

    変数(Variable)の補完機能を追加

---
+++ src/containers/ruby-tab/variable-completer.js
@@ -0,0 +1,24 @@
+/**
+ * Define Ruby code completer for Variable Blocks
+ */
+class VariableCompleter {
+    getCompletions (editor, session, pos, prefix, callback) {
+        const words = [
+            {
+                value: 'show_variable("$変数")',
+                meta: '変数 変数 を表示する'
+            },
+            {
+                value: 'hide_variable("$変数")',
+                meta: '変数 変数 を隠す'
+            },
+        ];
+        const completions = [];
+        words.forEach(
+            w => completions.push(w)
+        );
+        callback(null, completions);
+    }
+}
+
+export default VariableCompleter;
