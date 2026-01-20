9a2f5ccb00bb112aad71ab83c1030d97adfc87b8
Kouji Takao <kouji.takao@gmail.com>
2026-01-20 19:15:15 +0900

    fix: suppress and resolve console warnings
    - Improve log-suppression to check all console arguments
    - Add GenerateSW warning to ignoredWarnings
    - Add mobile-web-app-capable meta tag to address deprecation warning

--- src/lib/log-suppression.js
+++ src/lib/log-suppression.js
@@ -4,7 +4,8 @@
 
 const ignoredWarnings = [
     'Object freezing is not supported by Opal',
-    'Canvas2D: Multiple readback operations using getImageData are faster with the willReadFrequently attribute set to true',
+    'Canvas2D: Multiple readback operations using getImageData are faster ' +
+        'with the willReadFrequently attribute set to true',
     'Support for defaultProps will be removed from function components',
     'React does not recognize the colorMode prop on a DOM element',
     'React does not recognize the showNewFeatureCallouts prop on a DOM element',
@@ -19,28 +20,34 @@ const ignoredWarnings = [
     'componentWillReceiveProps has been renamed',
     'findDOMNode is deprecated',
     'The AudioContext was not allowed to start',
-    'apple-mobile-web-app-capable'
+    'apple-mobile-web-app-capable',
+    'GenerateSW has been called multiple times'
 ];
 
 /**
  * Check if a message should be ignored.
  * @param {string} message The message to check.
+ * @param {Array} args Additional arguments.
  * @returns {boolean} True if the message should be ignored.
  */
-const shouldIgnore = message => {
-    if (typeof message !== 'string') return false;
-    return ignoredWarnings.some(ignored => message.includes(ignored));
+const shouldIgnore = (message, ...args) => {
+    const allStrings = [message, ...args].filter(arg => typeof arg === 'string');
+    return allStrings.some(str =>
+        ignoredWarnings.some(ignored => str.includes(ignored))
+    );
 };
 
+/* eslint-disable no-console */
 const originalWarn = console.warn;
 const originalError = console.error;
 
 console.warn = function (message, ...args) {
-    if (shouldIgnore(message)) return;
+    if (shouldIgnore(message, ...args)) return;
     originalWarn.apply(console, [message, ...args]);
 };
 
 console.error = function (message, ...args) {
-    if (shouldIgnore(message)) return;
+    if (shouldIgnore(message, ...args)) return;
     originalError.apply(console, [message, ...args]);
 };
+/* eslint-enable no-console */
